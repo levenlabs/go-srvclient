237206c67329f5b7c64f6127301b324b03090e1f
Brian Picciano <mediocregopher@gmail.com>
2015-09-23 09:20:45 -0600

    make srvclient binary errors go to stderr

--- srvclient/main.go
+++ srvclient/main.go
@@ -9,13 +9,13 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s <hostname>\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s <hostname>\n", os.Args[0])
 		os.Exit(1)
 	}
 
 	r, err := srvclient.SRV(os.Args[1])
 	if err != nil {
-		fmt.Printf("error resolving '%q': %s\n", os.Args[1], err)
+		fmt.Fprintf(os.Stderr, "error resolving '%q': %s\n", os.Args[1], err)
 		os.Exit(2)
 	}
 
